d4c267a566c4e67a9aea922b739ae562e1d45c94
Bogdan Mocanu <bogdan.game.development@gmail.com>
2024-05-13 19:05:07 +0200

    Ready for WFC

--- Assets/Scripts/RecognizeIsland.cs
+++ Assets/Scripts/RecognizeIsland.cs
@@ -7,11 +7,13 @@ public class RecognizeIsland : MonoBehaviour
 {
     private Tilemap map;
     private TileBase[] tiles;
+    [SerializeField] private GameObject obj;
 
     private void Start()
     {
+        Debug.Log("Tiles of island:" + name);
         map = GetComponent<Tilemap>();
-        map.CompressBounds();
+        //map.CompressBounds();
         tiles = map.GetTilesBlock(map.cellBounds);
         var cellPosition = map.WorldToCell(transform.position);
 
@@ -20,13 +22,45 @@ public class RecognizeIsland : MonoBehaviour
         for (var z = 0; z < map.cellBounds.size.z; z++)
         {
             var tile = tiles[map.cellBounds.size.x * map.cellBounds.size.y * z + map.cellBounds.size.x * y + x];
-            if (tile != null)
-                Debug.Log("Tile at: " + new Vector3Int(cellPosition.x + x, cellPosition.y + y, cellPosition.z + z));
+            if (tile)
+            {
+                var tilePos = new Vector3Int(cellPosition.x + x, cellPosition.y + y, cellPosition.z + z);
+                Debug.Log("Tile at: " + tilePos);
+            }
+            else
+            {
+                //Debug.Log("Tile empty: " + new Vector3Int(cellPosition.x + x, cellPosition.y + y, cellPosition.z + z));
+            }
         }
     }
 
     public void GenerateWFC()
     {
         //do something with tiles
+
+        var cellPosition = map.WorldToCell(transform.position);
+
+        for (var x = 0; x < map.cellBounds.size.x; x++)
+        for (var y = 0; y < map.cellBounds.size.y; y++)
+        for (var z = 0; z < map.cellBounds.size.z; z++)
+        {
+            var tile = tiles[map.cellBounds.size.x * map.cellBounds.size.y * z + map.cellBounds.size.x * y + x];
+            if (tile)
+            {
+                var tilePos = new Vector3Int(cellPosition.x + x, cellPosition.y + y, cellPosition.z + z);
+                var data = new TileData();
+                tile.GetTileData(tilePos, map, ref data);
+
+                var tileObj = Instantiate(obj);
+                tileObj.transform.parent = map.transform;
+
+                tileObj.transform.position = map.CellToWorld(tilePos + map.origin) + map.tileAnchor;
+
+
+                tileObj.name = tilePos.ToString();
+            }
+        }
+
+        map.ClearAllTiles();
     }
 }
---
+++ Assets/Scripts/Utility/UnityEventsParam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine.Events;
+using UnityEngine;
+
+namespace Assets.Scripts.Utility
+{
+    [Serializable]
+    public class TransformEvent : UnityEvent<Transform>
+    {
+    }
+
+    [Serializable]
+    public class Vector3Event : UnityEvent<Vector3>
+    {
+    }
+
+
+    [Serializable]
+    public class StringEvent : UnityEvent<string>
+    {
+    }
+
+    [Serializable]
+    public class IntEvent : UnityEvent<int>
+    {
+    }
+
+    [Serializable]
+    public class FloatEvent : UnityEvent<float>
+    {
+    }
+}
